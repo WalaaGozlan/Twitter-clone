4bc3bbf20bed309651d083094a70cdd0cd53589a
WalaaGozlan <wlogh.89@gmail.com>
2021-02-23 03:26:40 +0200

    authentication-feature-signup

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import './App.css';
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
-import Widgets from "./Widgets"
+import Widgets from "./Widgets";
+import Signup from "./SignUp";
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
 
 function App() {
   return (
     //BEM 
+    <AuthProvider>
     <div className="app">
+      
       <Sidebar /> 
       <Feed />
       <Widgets />
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight:"100vh" }}>
+      <div className="w-100" style={{ maxWidth:"400px"}}>
+      <Signup />
+      </div>
+      </Container>
     </div>
-    
+    </AuthProvider>
   );
 }
 
--- twitter-clone/src/firebase.js
+++ twitter-clone/src/firebase.js
@@ -1,16 +1,20 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase"
+import firebase from "firebase";
+//
+import "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAukVa1eroth2Se5QqoPVzxDCMah7NAiBk",
-    authDomain: "twitter-clone-55f66.firebaseapp.com",
-    projectId: "twitter-clone-55f66",
-    storageBucket: "twitter-clone-55f66.appspot.com",
-    messagingSenderId: "374103506779",
-    appId: "1:374103506779:web:de28eef018e6ccfcb3247e",
-    measurementId: "G-10RGD8PXG5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
+
+  export const auth = firebaseApp.auth();
   export default db;
