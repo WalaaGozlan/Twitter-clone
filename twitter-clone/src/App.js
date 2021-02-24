dc0b3870c7db13172fce4b64b1511fc54a7031eb
WalaaGozlan <wlogh.89@gmail.com>
2021-02-24 03:32:46 +0200

    auth-login

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -4,25 +4,36 @@ import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 import Signup from "./SignUp";
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "./contexts/AuthContext"
+import Login from "./Login";
+
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     //BEM 
-    <AuthProvider>
+    <Router>
     <div className="app">
-      
-      <Sidebar /> 
+      {/* <Sidebar /> 
       <Feed />
-      <Widgets />
+      <Widgets /> */}
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight:"100vh" }}>
+      <AuthProvider>
+        <Switch>
+        {/* <Route path="/" components={{Sidebar:Sidebar , Feed:Feed, Widgets:Widgets}} /> */}
+        {/* <Route path="/" component={Feed} />
+        <Route path="/" component={Widgets} /> */}
       <div className="w-100" style={{ maxWidth:"400px"}}>
-      <Signup />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
       </div>
+      
+      </Switch>
+      </AuthProvider>
       </Container>
     </div>
-    </AuthProvider>
+    </Router>
   );
 }
 
--- twitter-clone/src/contexts/AuthContext.js
+++ twitter-clone/src/contexts/AuthContext.js
@@ -5,19 +5,25 @@ import { auth } from "../firebase"
 const AuthContext = React.createContext()
 
 export function useAuth(){
-    useContext(AuthContext)
+    return useContext(AuthContext)
 }
 
 export function AuthProvider({ children }){
-    const[currentUser, setCurrentUser] = useState();
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
 
     function signup(email, password){
         return auth.createUserWithEmailAndPassword(email, password)
     }
 
+    function login(email, password){
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
+            setLoading(false);
     })
 
     return unsubscribe;
@@ -26,12 +32,13 @@ export function AuthProvider({ children }){
     
     const value = {
         currentUser,
+        login,
         signup
     }
 
     return (
         <AuthContext.Provider value={value}>
-            {children}
+            { !loading && children }
         </AuthContext.Provider>
     )
 }
